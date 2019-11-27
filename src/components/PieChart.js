d3ef60194c6d2dd6c73aabeb7ec37e2a53fa28e7
kim-ly <SoSe2018>
2019-11-27 11:32:26 +0100

    Adapted labels for pie chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -27,11 +27,6 @@ render() {
                       onPress: () => {
                         return [
                           {
-                            target: "data",
-                            mutation: ({ style }) => {
-                              return style.fill === "#c43a31" ? null : { style: { fill: "#c43a31" } };
-                            }
-                          }, {
                             target: "labels",
                             mutation: ({ text }) => {
                               return text === "clicked" ? null : { text: "clicked" };
@@ -49,16 +44,16 @@ render() {
                   }}
                 innerRadius={({ datum }) => datum.y }
                 data={[
-                    {x: "1", y: smileyRangeCount[0], labels: "Smiley Range 1" },
-                    {x: "2", y: smileyRangeCount[1], labels: "Smiley Range 2"},
-                    {x: "3", y: smileyRangeCount[2], labels: "Smiley Range 3"},
-                    {x: "4", y: smileyRangeCount[3], labels: "Smiley Range 4"},
-                    {x: "5", y: smileyRangeCount[4], labels: "Smiley Range 5"},
-                    {x: "6", y: smileyRangeCount[5], labels: "Smiley Range 6"},
-                    {x: "7", y: smileyRangeCount[6], labels: "Smiley Range 7"},
-                    {x: "8", y: smileyRangeCount[7], labels: "Smiley Range 8"},
-                    {x: "9", y: smileyRangeCount[8], labels: "Smiley Range 9"},
-                    {x: "10", y: smileyRangeCount[9], labels: "Smiley Range 10"}
+                    {x: "1", y: smileyRangeCount[0], label: "Smiley Range 1" },
+                    {x: "2", y: smileyRangeCount[1], label: "Smiley Range 2"},
+                    {x: "3", y: smileyRangeCount[2], label: "Smiley Range 3"},
+                    {x: "4", y: smileyRangeCount[3], label: "Smiley Range 4"},
+                    {x: "5", y: smileyRangeCount[4], label: "Smiley Range 5"},
+                    {x: "6", y: smileyRangeCount[5], label: "Smiley Range 6"},
+                    {x: "7", y: smileyRangeCount[6], label: "Smiley Range 7"},
+                    {x: "8", y: smileyRangeCount[7], label: "Smiley Range 8"},
+                    {x: "9", y: smileyRangeCount[8], label: "Smiley Range 9"},
+                    {x: "10", y: smileyRangeCount[9], label: "Smiley Range 10"}
                 ]}
                 colorScale={[
                     "#D85F49",
